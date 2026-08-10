245db71d80ab4aba9747771a191c106352aeb9ec
Bail <2915289604@qq.com>
2026-08-10 18:07:46 +0800

    fix(core): 修复依赖歧义问题

--- src/cquptddl/core/db.py
+++ src/cquptddl/core/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlmodel import SQLModel
 
-from . import config
+from .config import config
 
 engine = create_async_engine(config.DATABASE_URL)
 
