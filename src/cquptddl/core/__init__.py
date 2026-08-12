32be8b1598823745c6d1a7a6448c76dda2f1ebcc
Bail <2915289604@qq.com>
2026-08-12 11:31:57 +0800

    refactor(core): 简化符号表设计

--- src/cquptddl/core/__init__.py
+++ src/cquptddl/core/__init__.py
@@ -1,11 +1,8 @@
-from collections.abc import Callable
-from typing import Any
-
 from . import db, task  # noqa: F401
 from .config import config
 from .event_bus import bus
 from .factory import get_client, get_session
-from .symbol import SymbolTable
+from .symbol import call, export
 
 __all__ = [
     "bus",
@@ -17,32 +14,3 @@ __all__ = [
     "get_session",
     "task",
 ]
-_symbol_table = SymbolTable()
-
-
-def export(name: str, func: Callable):
-    _symbol_table.register(name, func)
-
-
-def call(name: str, *args, **kw) -> Any:
-    return _symbol_table.call(name, *args, **kw)
-
-
-def get_current_user_cookie():
-    return {
-        "route": "36c256fc918f0bb5b2115e9adfc1816b",
-        "JSESSIONID": "657F53C1A29698F90DE325463C1E3791",
-        "CASTGC": "TGT-291372-BhBVbXyfikVGMP4G2N-70YzYU1ypy-f9ret2N63YDlunlRfveeovDeNsVlfmFjz2OE8null_main",
-        "happyVoyage": "4KRY3KVzWbslCYAGrGtRCWvo8mANaqsqpOBIJmH6rcDBTXawwTUcVY98EquMaFCb7c6k41R1CmdXnPHbchnsBoWAY7CSDUxd/7JJwjt+DrL9vzJGon6srzhtTLZpBGt+S77KNLfZ/JrEey/TCopcCkGI7OLVfvOUoG2gEjjYZvI=",
-        "platformMultilingual": "zh_CN",
-    }
-    return {
-        "route": "bca4c666c8d521e78cedf949592f469e",
-        "JSESSIONID": "3BF55C8D06F0284ECD55F273CE3E093C",
-        "CASTGC": "TGT-317821-2xhuEE3Sx5IsRSUmeVlCMtJTrqgqvV9Zj-FqtCCB75gJJxjC04q-EoJzS-NlwGR-vKwnull_main",
-        "happyVoyage": "4vsPjxKTwoJuBuKZWC1DuMl+q26/mNH3sSxvr768mjpgrmFbwKEAP+lttYn/zUVNGICTAkn/JQrQcUulkm9G7p6suHXQvyIzZEVRiKk05qV6/QK41U1aonrSuULwL9evk0O10s1y0mcPDpCqBuVbSYCLlutttpQvP55lecEDgfc=",
-        "platformMultilingual": "zh_CN",
-    }
-
-
-export("auth.get_current_user_cookie", get_current_user_cookie)
--- src/cquptddl/core/symbol.py
+++ src/cquptddl/core/symbol.py
@@ -1,17 +1,14 @@
 from collections.abc import Callable
 from typing import Any
 
+_symbol_table: dict[str, Callable] = {}
 
-class SymbolTable:
-    _table: dict[str, Callable]
 
-    def __init__(self):
-        self._table = {}
+def export(name: str, func: Callable):
+    if name in _symbol_table:
+        raise NameError(f"符号{name}已存在")
+    _symbol_table[name] = func
 
-    def register(self, name: str, func: Callable):
-        if name in self._table:
-            raise NameError(f"符号{name}已存在")
-        self._table[name] = func
 
-    def call(self, name: str, *args, **kw) -> Any:
-        return self._table[name](*args, **kw)
+def call(name: str, *args, **kw) -> Any:
+    return _symbol_table[name](*args, **kw)
