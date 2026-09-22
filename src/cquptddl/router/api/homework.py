fc909bd6111a1d7d8ef130adc7638a1c054def9c
Bail <2915289604@qq.com>
2026-09-22 18:09:11 +0800

    fix(homework): 修复刷新作业时异常捕获不完全的问题

--- src/cquptddl/router/api/homework.py
+++ src/cquptddl/router/api/homework.py
@@ -7,7 +7,9 @@ from fastapi import APIRouter, Depends, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from cquptddl import core
-from cquptddl.exc import InvalidPlatformCookie, PlatformNotBound, RefreshCoolingDown
+from cquptddl.exc import (
+    CquptddlException,
+)
 from cquptddl.middleware.auth import need_login
 from cquptddl.model.db import User
 from cquptddl.model.schema.homework import (
@@ -72,7 +74,7 @@ async def refresh(
         for p in PlatformEnum:
             try:
                 await core.call("homework.refresh_homework", session, user, p)
-            except (PlatformNotBound, InvalidPlatformCookie, RefreshCoolingDown) as e:
+            except CquptddlException as e:
                 prompts.append(
                     REFRESH_HOMEWORK_PROMPT_TEMPLATE.format(platform=p, info=e)
                 )
